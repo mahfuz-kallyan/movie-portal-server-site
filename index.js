c255dfef2f5eb05a40a009e2ff952af56c890615
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2024-12-12 03:16:30 +0600

    added delete from server side

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -36,10 +36,18 @@ async function run() {
       res.send(result)
     })
 
+   
     app.post('/movies', async (req, res) => {
       const newMovie = req.body
       console.log(newMovie);
       const result = await movieCollection.insertOne(newMovie)
+      res.send(result)
+    })
+
+    app.delete('/movies/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await movieCollection.deleteOne(query)
       res.send(result)
     })
 

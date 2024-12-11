cdaa9f0ca304d0e1975df1a2efa244533f0cebc2
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2024-12-11 21:22:33 +0600

    added data to server side

--- index.js
+++ index.js
@@ -26,21 +26,38 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const movieCollection = client.db('movieDB').collection('movies')
+
+
+    app.get('/movies', async(req, res)=>{
+      const cursor = movieCollection.find();
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post('/movies', async (req, res) => {
+      const newMovie = req.body
+      console.log(newMovie);
+      const result = await movieCollection.insertOne(newMovie)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
-    res.send('Movie Portal Server Running')
+app.get('/', (req, res) => {
+  res.send('Movie Portal Server Running')
 })
 
-app.listen(port, ()=>{
-    console.log(`Movie Server running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`Movie Server running on port: ${port}`);
 })

9832c7fbe67728ab41d3baf600946737c4b7a0f9
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2025-05-19 20:09:33 +0600

    added delete function

--- index.js
+++ index.js
@@ -68,15 +68,27 @@ async function run() {
     })
 
     // users related api
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user.email }
       const existingUser = await userCollection.findOne(query)
       if (existingUser) {
-        return res.send({message: 'User already exists', insertedId: null})
+        return res.send({ message: 'User already exists', insertedId: null })
       }
       const result = await userCollection.insertOne(user)
       res.send(result)
+    })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query)
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
